23f9ecd2ac38834b1c64c261c07935a1fd14b292
Robin Liu <i@greenvine.me>
2018-07-14 18:58:03 +0300

    Remove pjson dependency

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,3 +1,19 @@
-import * as pjson from 'pjson';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
 
-export const getSdkVersion = () => pjson.version;
+export const getSdkVersion = () => {
+  const pkgRoot = resolve(__dirname, '..', '..');
+  const pkgContents = readFileSync(`${pkgRoot}/package.json`);
+
+  try {
+    const json = JSON.parse(pkgContents.toString());
+
+    if (json && json.version) {
+      return json.version;
+    } else {
+      throw new Error('Version identifier does not exist');
+    }
+  } catch (e) {
+    return '-1';
+  }
+};
